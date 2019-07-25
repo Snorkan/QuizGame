7a5d4256263309b97857b72dee56ff450d9032e2
Elinor Löverli <elinor.loverli@folksam.se>
2019-07-25 13:01:37 +0200

    Add program

---
+++ QuizGame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace QuizGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*string userName;
+            Console.WriteLine("Hi, write your name plz.");
+            userName = Console.ReadLine();
+            Console.WriteLine($"Hello {userName}, let's start playing hehehe."); */
+
+            string fileName = @"C:\QuizGame\QuestionAnswer.json";
+
+            string jsonTxt = string.Empty;
+           
+            using (StreamReader r = new StreamReader(fileName))
+            {
+                var json = r.ReadToEnd();
+                jsonTxt = json.ToString();
+            }
+            
+            var questionCollection = JsonConvert.DeserializeObject<QuestionCollection>(jsonTxt);
+
+
+            Questions question = new Manager().ChooseQuestion(questionCollection, 1);
+
+            new Manager().Loopiloop(question, 3);
+            
+
+        }
+    }
+}
+
+
