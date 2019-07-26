86d16d746d6862b6ec16ba089dd36ffb0c15f26a
Elinor Löverli <elinor.loverli@folksam.se>
2019-07-26 10:19:34 +0200

    Add method that initiate a question based on question id

---
+++ QuizGame/QuestionCollectionManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizGame
+{
+    class QuestionCollectionManager
+    {
+        public void ReturnQuestion(QuestionCollection questionCollection, int questionNumber)
+        {
+            Questions question = new Manager().ChooseQuestion(questionCollection, questionNumber);
+            new Manager().Loopiloop(question, 3);
+        } 
+        
+    }
+}
