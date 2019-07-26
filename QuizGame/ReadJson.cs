1d453b950efe8f637632315c516ae44bc04ff227
Elinor Löverli <elinor.loverli@folksam.se>
2019-07-26 17:00:22 +0200

    Moved the process of reading and deserializing Json into a class

---
+++ QuizGame/ReadJson.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace QuizGame
+{
+    public class ReadJson
+    {   
+        public QuestionCollection QC(string fileName)
+        {
+            string jsonTxt = string.Empty;
+            using (StreamReader r = new StreamReader(fileName))
+            {
+                var json = r.ReadToEnd();
+                jsonTxt = json.ToString();
+            }
+
+            var questionCollection = JsonConvert.DeserializeObject<QuestionCollection>(jsonTxt);
+            return questionCollection;
+        }   
+    }
+}
