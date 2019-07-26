d12fcf0a48cf1e2e79203c1399d4d59d025c3a56
Elinor Löverli <elinor.loverli@folksam.se>
2019-07-26 17:01:19 +0200

    Removed json logic

--- QuizGame/Program.cs
+++ QuizGame/Program.cs
@@ -17,20 +17,7 @@ namespace QuizGame
             Console.WriteLine($"Hello {userName}, let's start playing hehehe."); */
 
             string fileName = @"C:\QuizGame\QuestionAnswer.json";
-            //QuestionCollection questionCollection = new ReadJson().QC(fileName);
-
-            /*List<int> usedQuestions = new List<int>();
-            int randomNumber = new Random().Next(1, maxInt);
-            usedQuestions.Add(randomNumber);*/
-            
-            string jsonTxt = string.Empty;
-            using (StreamReader r = new StreamReader(fileName))
-            {
-                var json = r.ReadToEnd();
-                jsonTxt = json.ToString();
-            }
-
-            var questionCollection = JsonConvert.DeserializeObject<QuestionCollection>(jsonTxt);
+            QuestionCollection questionCollection = new ReadJson().QC(fileName);
 
             int maxInt = questionCollection.Questions.Count + 1;
 
@@ -39,33 +26,21 @@ namespace QuizGame
             // question id 1, 2, 3, 4 ... maxInt
             List<int> questionId = new Generator().QuestionIds(maxInt);
 
-            
             for (int i = 1; i < maxInt; i++)
             {
                 int l = questionId.Count;
-
-                Console.WriteLine($"items in list: {l}");
-
                 int randomNumber = new Random().Next(0, l);
 
-                Console.WriteLine($"Random number in loop: {randomNumber}");
-                // remove at index randomnumber
-                
                 int[] scoreTracker = new QuestionCollectionManager().ReturnQuestion(questionCollection, questionId[randomNumber]);
 
                 questionId.RemoveAt(randomNumber);
-
-                Console.WriteLine($"items left: {l}");
-
                 score += scoreTracker[1];
-                Console.WriteLine($"Your total score is: {score}!!");
+
+                if (l == 1) {
+                    Console.WriteLine($"Your total score is: {score}!!");
+                }
+                
             }
-            
-
-            
-            
-
-
         }
     }
 }
