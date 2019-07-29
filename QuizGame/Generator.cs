3af25551b3be1fd3ce759ee638a52a3f14bbc3d2
Elinor Löverli <elinor.loverli@folksam.se>
2019-07-29 15:17:03 +0200

    Move score tracker

--- QuizGame/Generator.cs
+++ QuizGame/Generator.cs
@@ -15,8 +15,34 @@ namespace QuizGame
             {
                 numbers.Add(i);
             }
-
             return numbers;
         }
+
+        public int ScoreTracker(QuestionCollection questionCollection)
+        {
+            int maxInt = questionCollection.Questions.Count + 1;
+
+            // question id 1, 2, 3, 4 ... maxInt
+            List<int> questionId = new Generator().QuestionIds(maxInt);
+            int score = 0;
+            for (int i = 1; i < maxInt; i++)
+            {
+                int l = questionId.Count;
+                int randomNumber = new Random().Next(0, l);
+
+                int[] scoreTracker = new QuestionCollectionManager().ReturnQuestion(questionCollection, questionId[randomNumber]);
+
+                questionId.RemoveAt(randomNumber);
+                score += scoreTracker[1];
+
+                if (l == 1)
+                {
+                    Console.WriteLine($"Your total score is: {score}!!");
+                }
+
+            }
+            return score;
+        }
+
     }
 }
