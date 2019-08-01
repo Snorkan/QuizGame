686011404d2784a130206f910639365b8e52a2ef
Elinor Löverli <elinor.loverli@folksam.se>
2019-08-01 17:26:48 +0200

    Methods to update the highscore list. Remove name if already exists. Add new object with name + score

---
+++ QuizGame/UpdateUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace QuizGame
+{
+    public class UpdateUser
+    {
+        public int ReturnScoreIfNameExists(UserList userList, string userName)
+        {
+            int score = 0;
+
+            for (int index = userList.Users.Count - 1; 
+                index >= 0; index--)
+            {
+                if (userList.Users.ElementAt(index).Name == userName)
+                {
+                    score = userList.Users.ElementAt(index).Score;
+                    userList.Users.RemoveAt(index);
+                }
+            }
+            return score;
+        }
+
+        public void AddScore(UserList userList, string userName, int score)
+        {
+            User newUser = new User()
+            {
+                Name = userName,
+                Score = score
+            };
+            userList.Users.Add(newUser);
+        }
+    }
+}
