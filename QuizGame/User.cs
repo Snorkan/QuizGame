d12cea041eca7526bbd3a49631640289d6cc4372
Elinor Löverli <elinor.loverli@folksam.se>
2019-08-01 17:23:18 +0200

    Highscore tracker - add json file and classes to deserialize the object

---
+++ QuizGame/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizGame
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+
+}
+
---
+++ QuizGame/UserList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuizGame
+{
+    public class UserList
+    {
+        public List<User> Users { get; set; }
+    }
+}
